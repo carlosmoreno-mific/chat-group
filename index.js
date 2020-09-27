3e0a81655f3d2e7dafde818a156d9ce9d5665761
Carlos Moreno <carlosmoreno-mific>
2020-09-27 10:49:23 -0600

    ended template

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("hamburguer");
 const closeBtn = document.getElementById("close-button");
 const nav = document.getElementById("sidebar-container");
+const btnToggleDropdown = document.getElementById('btn-toggle-dropdown');
+const dropdownContent = document.getElementById('dropdown-content');
 
 btn.addEventListener("click", () => {
     nav.classList.toggle("active");
@@ -11,3 +13,16 @@ closeBtn.addEventListener("click", () => {
     nav.classList.toggle("active");
     btn.classList.toggle("active");
 });
+
+btnToggleDropdown.addEventListener('click', (e) => {
+    e.stopPropagation();
+    dropdownContent.classList.toggle('show');
+});
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+        }
+    }
+}
